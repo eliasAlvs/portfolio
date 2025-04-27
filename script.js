373092b78746c082c83f5ff0ccad15a1b43b9442
eliasAlvs <137322132+elijahAlvs@users.noreply.github.com>
2025-04-27 01:28:56 -0300

    Contatos atualizados
    Atualizei as informações de contato e adicionei o link do meu github e linkedin nos botões.

---
+++ script.js
@@ -0,0 +1,9 @@
+let gHub = document.querySelector("#gtHub");
+let linkedIn = document.querySelector("#lnkdIn");
+
+gHub.addEventListener("click", ()=>{
+    window.open("https://github.com/eliasAlvs");
+});
+linkedIn.addEventListener("click", ()=>{
+    window.open("https://www.linkedin.com/in/elias-alves-lima-9a0980362/");
+});
